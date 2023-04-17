7ec7838021aac0b83ee13370c76c50ef68c9a4dc
Sohyun <sh-119@naver.com>
2023-04-17 19:02:06 +0900

    날씨 저장스케쥴링 API 개발

--- src/main/java/com/test/weather/service/DiaryService.java
+++ src/main/java/com/test/weather/service/DiaryService.java
@@ -1,13 +1,17 @@
 package com.test.weather.service;
 
+import com.test.weather.domain.DateWeather;
 import com.test.weather.domain.Diary;
+import com.test.weather.repository.DateWeatherRepository;
 import com.test.weather.repository.DiaryRepository;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser; // *
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.BufferedReader;
@@ -25,27 +29,59 @@ public class DiaryService {
     @Value("${openweathermap.key}") //application.yml 에 저장해 놓고 가져와서 쓰기!!!
     private String apiKey;
     private final DiaryRepository diaryRespository;
+    private final DateWeatherRepository dateWeatherRepository;
 
-    public DiaryService(DiaryRepository diaryRespository) {
+    public DiaryService(DiaryRepository diaryRespository, DateWeatherRepository dateWeatherRepository) {
         this.diaryRespository = diaryRespository;
+        this.dateWeatherRepository = dateWeatherRepository;
     }
 
-    public void createDiary(LocalDate date, String text){
+    // 매일 한 시마다 캐싱을 위한 날씨데이터를 저장하는 스케쥴링 작성
+    @Transactional
+    @Scheduled(cron = "0 0 1 * * *")
+    public void saveWeatherDate(){
+        dateWeatherRepository.save(getWeatherFromApi());
+    }
+
+    private DateWeather getWeatherFromApi(){
         // open weather map 에서 날씨 데이터 가져오기 ***
         String weatherData = getWeatherString();
 
         // 받아온 날씨 json 파싱하기
         Map<String, Object> parseWeather = parseWeather(weatherData);
 
+        DateWeather dateWeather = new DateWeather();
+        dateWeather.setDate(LocalDate.now());
+        dateWeather.setWeather(parseWeather.get("main").toString());
+        dateWeather.setIcon(parseWeather.get("icon").toString());
+        dateWeather.setTemperature((Double) parseWeather.get("temp"));
+
+        return dateWeather;
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void createDiary(LocalDate date, String text){
+
+        // 날씨 데이터 가져오기 (API 에서 or DB에서)
+        DateWeather dateWeather = getDateWeather(date);
+
         // 파싱된 데이터 + 일기 값 우리 db에 넣기
         Diary nowDiary = new Diary();
-        nowDiary.setWeather(parseWeather.get("main").toString());
-        nowDiary.setIcon(parseWeather.get("icon").toString());
-        nowDiary.setTemperature((Double)parseWeather.get("temp"));
+        nowDiary.setDateWeather(dateWeather);
         nowDiary.setText(text);
-        nowDiary.setDate(date);
 
         diaryRespository.save(nowDiary);
+    }
+
+    private DateWeather getDateWeather(LocalDate date){
+        List<DateWeather> dateWeatherListFromDB = dateWeatherRepository.findAllByDate(date);
+
+        // 새로 api 에서 날씨 정보를 가져와야함 -> 오늘 날씨 정보로 대체해서 서버에서 가져오기로한다.
+        if(dateWeatherListFromDB.size() == 0){
+            return getWeatherFromApi();
+        }else{
+            return dateWeatherListFromDB.get(0);
+        }
     }
 
     @Transactional(readOnly = true) // readOnly = true 로 해줌으로써 속도성능을 높여준다.
